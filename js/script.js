13d8bc9c98ca9f0837131007e78f3851831ab996
AnaCascante <ana.cascante@stud.noroff.no>
2022-10-08 11:09:17 +0200

    task 3 lesson 3
    done

---
+++ js/script.js
@@ -0,0 +1,113 @@
+// Question 1  Console log the price
+
+var product = {
+    name: "Chicken feet",
+    price: 149.99, 
+}; 
+
+console.log (product.price); 
+
+// Question 2   Loop through the array below and console log each colour property.
+
+var animals = [
+	{
+		type: "cat",
+		colour: "black",
+	},
+	{
+		type: "elephant",
+		colour: "grey",
+	},
+	{
+		type: "dog",
+		colour: "brown",
+	},
+	{
+		type: "zebra",
+		colour: "black and white",
+	},
+];
+
+for (var i= 0; i < animals.length; i++) {
+    console.log (animals [i].colour)
+}
+
+// Question 3 Loop through the array below and console log each name property only if it is on special.
+
+var dinnerOptions = [
+	{ name: "Fish and Dips", price: 11.95, onSpecial: true },
+	{ name: "Bacon and Frogs", price: 12.99, onSpecial: false },
+	{ name: "Mince Flies", price: 19.0, onSpecial: true },
+];
+
+for (var i=0; i <dinnerOptions.length; i++) {
+    if (dinnerOptions [i].onSpecial){
+        console.log (dinnerOptions [i].name);
+    }
+}
+
+
+// Question 4 Loop through the array below and log each person's name and surname if they are between the ages of 20 and 50 (including 20 and 50):
+
+var people = [
+	{
+		firstName: "Burt",
+		lastName: "Smacharach",
+		age: 35,
+	},
+	{
+		firstName: "Elvis",
+		lastName: "Deadly",
+		age: 55,
+	},
+	{
+		firstName: "Susan",
+		lastName: "Sarongson",
+		age: 42,
+	},
+	{
+		firstName: "Big",
+		lastName: "Gabe",
+		age: 20,
+	},
+];
+
+for (var i=0; i <people.length; i++){
+    var age = people [i].age; 
+    var fullName = people[i].firstName + " " + people[i].lastName; 
+    
+    if (age >=20 && age <=50){
+        console.log (fullName);
+    }
+}
+
+// Question 5  Select the ul element in the HTML file, loop through the array below and create a li element containing the product name and price properties.
+
+var products = [
+	{
+		name: "Elephant eggs",
+		price: 45.0,
+	},
+	{
+		name: "Bucket of water",
+		price: 459.99,
+	},
+	{
+		name: "4 cabbages",
+		price: 500.95,
+	},
+];
+
+var productContainer = document.querySelector ("ul")
+
+for (var i = 0; i < products.length; i++){
+    productContainer.innerHTML += "<li>" + products[i].name + " - " + products[i].price + "</li>";
+}
+
+// Your final HTML should look like this:
+
+//<ul>
+//	<li>Elephant eggs - 45.00</li>
+//	<li>Bucket of water - 459.99</li>
+//	<li>4 cabbages - 500.95</li>
+//</ul> */
